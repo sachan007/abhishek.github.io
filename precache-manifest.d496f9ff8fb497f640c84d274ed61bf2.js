3b9a7f586fa2cdf3785d7be3125839749dac048a
Ubuntu <ubuntu@ip-172-31-22-171.eu-central-1.compute.internal>
2022-02-01 21:37:59 +0000

    wdwdwd

---
+++ precache-manifest.d496f9ff8fb497f640c84d274ed61bf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43637312243d3f25979b1f4d43ce491c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64fd14a13422216bec21",
+    "url": "/static/js/2.f9128138.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.f9128138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a343374388bca6f4670b",
+    "url": "/static/js/main.72d67b97.chunk.js"
+  },
+  {
+    "revision": "0b4f3269348c2e5315e5",
+    "url": "/static/js/runtime-main.9089ee33.js"
+  }
+]);
